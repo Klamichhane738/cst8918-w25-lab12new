4ffda514ebf2f469712f0875abf1fe4320c01c94
Klamichhane738.Keshav <lami0053@algonquinlive.com>
2025-03-31 20:20:43 -0400

    Updated infra folder

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "app_rg" {
   name     = "lami0053-a12-rg"
   location = "East US"
- 
+
   tags = {
     environment = "test"
   }
--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -1,35 +1,35 @@
 terraform {
 
   required_version = "~> 1.5"
- 
+
   required_providers {
 
     azurerm = {
 
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
 
       version = "~> 3.0"
 
     }
 
   }
- 
+
   backend "azurerm" {
 
-    resource_group_name  = "lami0053-githubactions-rg"
+    resource_group_name = "lami0053-githubactions-rg"
 
     storage_account_name = "lami0053githubactions"
 
-    container_name       = "tfstate"
+    container_name = "tfstate"
 
-    key                  = "prod.app.tfstate"
+    key = "prod.app.tfstate"
 
-    use_oidc             = true
+    use_oidc = true
 
   }
 
 }
- 
+
 provider "azurerm" {
 
   features {}
